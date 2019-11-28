35713cb73e435d28caef6adfdbaa98694364a882
Marcus Hirt <marcus@hirt.se>
2019-11-28 11:22:26 +0100

    Running spotless on all

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh -l
 
 set -e
-echo "==========Entering core============="
-cd core
-echo "==========Running spotless=========="
+echo "======== Building p2 repo ==================="
+cd releng/third-party
+sh -c "mvn p2:site"
+sh -c "nohup mvn jetty:run &"
+echo "======== Entering core ======================"
+cd ../../core
+echo "======== Running spotless for core =========="
 sh -c "mvn spotless:check"
-echo "==========Finished=================="
+echo "======== Running spotless for application ==="
+cd ..
+sh -c "mvn -Puitests spotless:check"
+echo "======== Finished ===================="
